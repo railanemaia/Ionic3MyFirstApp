9ea8f86444dab5e5c0d669151fb41d95df3202fa
Railane Maia <railanemaia@Railanes-MacBook-Air.local>
2017-06-30 16:35:31 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { IntroPage } from "../pages/intro/intro";
 
 @Component({
   templateUrl: 'app.html'
